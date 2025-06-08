ccbb46273ea16bb628273726c6b45bd8d29e7ba7
Jordan Sibley <jordan.c.sibley@gmail.com>
2025-06-08 12:52:46 -0700

    Clean up repo

---
+++ R/p_wrapper.R
@@ -0,0 +1,8 @@
+p_wrapper <- function(rprey, alpha, eff, pmort, K, currpop, days, func) {
+  parms <- list(rprey = rprey, alpha = alpha, eff = eff, pmort = pmort, K = K)
+  result <- ode(y = currpop, times = days, func = func, parms = parms)
+  colnames(result) <- c("time", "prey", "pred")
+  # get metrics
+  metrics <- compute_metrics(as.data.frame(result))
+  return(metrics)
+}
